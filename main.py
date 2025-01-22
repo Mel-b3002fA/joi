c699d9a2ffab46c508f93b0444623d9f4da287c1
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-01-22 18:31:28 +0100

    push

--- main.py
+++ main.py
@@ -1,13 +1,103 @@
 # main.py
 
+""" 
 from chatterbot import ChatBot
 
 chatbot = ChatBot("Chatpot")
 
 exit_conditions = (":q", "quit", "exit")
 while True:
-    query = input(". ")
+    query = input(".")
     if query in exit_conditions:
         break
     else:
-        print(f"🪴 {chatbot.get_response(query)}")
+        print(f"🔰 {chatbot.get_response(query)}") 
+"""
+
+
+""" 
+
+from chatterbot import ChatBot
+chatbot = ChatBot("Ron Obvious")
+from chatterbot.trainers import ListTrainer
+
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+trainer = ListTrainer(chatbot)
+
+trainer.train(conversation)
+response = chatbot.get_response("Good morning!")
+print(response) 
+
+"""
+
+""" 
+from chatterbot import ChatBot
+bot = ChatBot('Norman')
+bot = ChatBot(
+    'Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    database_uri='sqlite:///database.sqlite3'
+)
+bot = ChatBot(
+    'Norman',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter'
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+while True:
+    try:
+        bot_input = bot.get_response(input())
+        print(bot_input)
+
+    except(KeyboardInterrupt, EOFError, SystemExit):
+        break 
+    
+    """
+
+
+
+from chatterbot import ChatBot
+
+
+# Uncomment the following lines to enable verbose logging
+import logging
+logging.basicConfig(level=logging.INFO)
+
+# Create a new instance of a ChatBot
+bot = ChatBot(
+    'Terminal',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch'
+    ],
+    database_uri='sqlite:///database.db'
+)
+
+print('Type something to begin...')
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        user_input = input()
+
+        bot_response = bot.get_response(user_input)
+
+        print(bot_response)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
